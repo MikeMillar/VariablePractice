2eb33ce9125486dfcdb0c7a292a01f883cca5171
Mike Millar <39285355+MikeMillar@users.noreply.github.com>
2019-07-14 17:08:41 -0600

    int variables

--- Variables/main.cpp
+++ Variables/main.cpp
@@ -3,9 +3,19 @@
 using namespace std;
 
 int main() {
-
+	
 	int numCats = 5;
+	cout << "Number of cats: " << numCats << endl;
 
+	int numDogs = 7;
+	cout << "Number of dogs: " << numDogs << endl;
+
+	int numAnimals = numCats + numDogs;
+	cout << "Total number of animals: " << numAnimals << endl;
+
+	cout << "New dog acquired!" << endl;
+	numDogs = numDogs + 1;
+	cout << "New number of dogs: " << numDogs << endl;
 
 
 
